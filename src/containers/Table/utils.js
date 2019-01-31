d90f47b7c8c4f461f684b0eeb251121c2284144c
pallavi-syook <pallavi@syook.com>
2019-01-31 17:43:55 +0530

    added filter query conditions

--- src/containers/Table/utils.js
+++ src/containers/Table/utils.js
@@ -1,6 +1,14 @@
+import _ from 'lodash';
+
 export const queryCondition = (attrValue, searchValue, query) => {
   switch (query) {
     case 'Contains': return attrValue.includes(searchValue);
+    case 'Does Not Contain': return attrValue.includes(searchValue);
+    case 'Is': return attrValue === searchValue;
+    case 'IsNot': return attrValue !== searchValue;
+    case 'Is Empty': return _.isEmpty(attrValue);
+    case 'Is Not Empty': return !_.isEmpty(attrValue);
+    default: return;
   }
 }
 
@@ -14,7 +22,7 @@ export const loopFilters = (data, filters) => {
   let filteredData = data
   filters.forEach(filter => {
     debugger
-    filteredData = andFunction(data, filter.attribute, filter.value, filter.query)
+    filteredData = andFunction(filteredData, filter.attribute, filter.value, filter.query)
   })
   return filteredData;
 }
--- src/containers/constants.js
+++ src/containers/constants.js
@@ -1,6 +1,6 @@
 const filterPredicates = ['And', 'Or']
 
-const filterQueries = ['Contains', 'Does Not Contain', 'Is', 'IsNot']
+const filterQueries = ['Contains', 'Does Not Contain', 'Is', 'IsNot', 'Is Empty', 'Is Not Empty']
 
 export const predicateOptions = filterPredicates.map(p => {
   return {value: p, label: p}
