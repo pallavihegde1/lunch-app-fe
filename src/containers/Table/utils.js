e53a0e23e6be9cffab6e106eeb45a736f81a0459
pallavi-syook <pallavi@syook.com>
2019-02-02 12:37:04 +0530

    or filter refactor

--- src/containers/Table/utils.js
+++ src/containers/Table/utils.js
@@ -18,24 +18,24 @@ export const filterFunction = (data, attr, searchValue, query) => {
 }
 
 export const loopFilters = (data, filters) => {
-  let filteredData = data
   if(filters.length === 1) {
      return filterFunction(data, filters[0].attribute, filters[0].value, filters[0].query)
   }
   else {
     if(filters[1].predicate === 'And') {
+      let filteredData = data
       filters.forEach((filter, index) => {
          filteredData = filterFunction(filteredData, filter.attribute, filter.value, filter.query)
-         return filteredData;
       })
+      return filteredData;
     }
     else if(filters[1].predicate === 'Or') {
-      const orFilteredData = []
+      let filteredData = []
       filters.forEach((filter, index) => {
-         filteredData = _.merge(orFilteredData, filterFunction(data, filter.attribute, filter.value, filter.query));
-         return filteredData;
+        const indexedFilter = filterFunction(data, filter.attribute, filter.value, filter.query)
+         filteredData = [...filteredData, ...indexedFilter];
       })
+      return filteredData;
     }
   }
-  return filteredData
 }
